147f92e280c4254e07ae273493645deca4346e87
Alexander Kalashnikov <akalashnikov@optimalcity.ru>
2023-11-30 14:28:12 +0300

    Usage

--- zipeg.c
+++ zipeg.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main()
+static int run(const char *);
+
+int main(int argc, char *argv[])
 {
-    printf("Hello, ZIpeg!\n");
+    const char *program = argv[0];
+    printf("%s -- determine whether the FILE is a Rarjpeg, i.e. the image and some zip-archive after it.\n", program);
+    if (argc < 2 || argc > 2)
+    {
+        printf("ERROR! input file must be the only argument.\nUsage:\n%s [FILE]\n", program);
+        return 1;
+    }
+
+    return run(argv[1]);
+}
+
+int run(const char *filename)
+{
+    printf("Looking inside of %s...\n", filename);
     return 0;
 }
