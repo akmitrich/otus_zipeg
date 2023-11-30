94b0d07c793550a39bb6aeaeb6bd92c40f0a8802
Alexander Kalashnikov <akalashnikov@optimalcity.ru>
2023-11-30 17:49:15 +0300

    Start of scan error

--- zipeg.c
+++ zipeg.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 static int run(const char *);
 
@@ -17,6 +19,47 @@ int main(int argc, char *argv[])
 
 int run(const char *filename)
 {
+    int n;
+    int length;
+    unsigned char jpeg_marker[2];
+    unsigned char le_bytes[2];
+    int16_t *int_marker = (int16_t *)jpeg_marker;
+
     printf("Looking inside of %s...\n", filename);
-    return 0;
+    FILE *input = fopen(filename, "rb");
+    if (input == NULL)
+    {
+        printf("ERROR: cannot open FILE %s\n", filename);
+        return 1;
+    }
+
+    n = fread(jpeg_marker, sizeof(jpeg_marker), 1, input);
+    if (n != 1 || (jpeg_marker[0] != 0xff && jpeg_marker[1] != 0xd8))
+    {
+        printf("ERROR: %s is not a JPEG. (%x %x)\n", filename, jpeg_marker[0], jpeg_marker[1]);
+        goto clean_up;
+    }
+
+    while (*int_marker != 0xffd9)
+    {
+        n = fread(jpeg_marker, sizeof(jpeg_marker), 1, input);
+        if (n != 1 || jpeg_marker[0] != 0xff)
+        {
+            printf("ERROR: %s missed JPEG app marker. (%x %x)\n", filename, jpeg_marker[0], jpeg_marker[1]);
+            goto clean_up;
+        }
+        n = fread(le_bytes, sizeof(le_bytes), 1, input);
+        length = le_bytes[1] | le_bytes[0] << 8;
+        printf("Marker: (%x, %x) -> %d\n", jpeg_marker[0], jpeg_marker[1], length);
+        if (fseek(input, length - 2, SEEK_CUR) != 0)
+        {
+            printf("ERROR: jump by wrong number of bytes: %d\n", length);
+            goto clean_up;
+        }
+    }
+
+    printf("TODO: it's a JPEG\n");
+clean_up:
+    fclose(input);
+    return n;
 }
